5c1a95053878dab3e5d2256d9bfe6a28a6918417
awidianto <agilwidianto15@gmail.com>
2026-01-10 01:35:32 +0700

    update db railway

--- config/database.go
+++ config/database.go
@@ -2,7 +2,7 @@ package config
 
 import (
 	"fmt"
-	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,13 +11,19 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "host=localhost user=postgres password=Agil@2002 dbname=bioskop_db port=5432 sslmode=disable"
-	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf(
+		"host=localhost user=postgres password=Agil@2002 dbname=bioskop_db port=5432 sslmode=disable",
+		os.Getenv("PGHOST"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+		os.Getenv("PGDATABASE"),
+		os.Getenv("PGPORT"),
+	)
 
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Gagal koneksi database")
+		panic("Gagal koneksi database")
 	}
 
 	DB = database
-	fmt.Println("Database connected")
 }
